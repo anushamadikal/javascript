e38326ff09a745e28317050e4c65a2a007abcb62
Anusha Poojary <anushamadikal@gmail.com>
2021-02-16 08:16:20 -0800

    Javascript repository created

---
+++ 05-Guess-My-Number/starter/game.js
@@ -0,0 +1,30 @@
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+var score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.message').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost!';
+    }
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
+
+//# sourceURL = script.js
